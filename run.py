60392aaf2f35f600fa6b858e2e9b5e9c2e6e16c1
Neil Van Alino <nvq.alino@gmail.com>
2024-03-29 23:28:53 +0800

    setup Retina, Chat classes

--- run.py
+++ run.py
@@ -7,13 +7,19 @@ output_dir.mkdir(parents=True, exist_ok=True)
 
 def go():
     segmenter = "yolo"
-    name = "coffee-shop"
+    name = "keiko-fat"
     ext = "jpg"
     img = f"./sample/{name}.{ext}"
     prompt = None
     output_img_filename = f"./output/{name}-{segmenter}"
 
-    results, model = vision.do_segment(img, segmenter=segmenter, prompt=prompt)
+    retina = vision.Retina(img)
+    # som = retina.generate_som_image()
+    # som.save(f"{output_img_filename}-som.{ext}")
+    # print(f"image saved: {output_img_filename}")
+
+    caption = retina.write_caption()
+    print(caption)
 
     # masked output
     # results[0].plot(
@@ -28,15 +34,6 @@ def go():
     # results[0].save_crop(
     #     save_dir="./output/crops", file_name=f"{name}-crop-{segmenter}"
     # )
-
-    # set SOM
-    som = vision.set_marks(img, results, font_size=50)
-    som.save(f"{output_img_filename}-som.{ext}")
-    print(f"image saved: {output_img_filename}")
-
-    # captioning
-    # caption = vision.write_caption(img)
-    # print(caption)
 
 
 if __name__ == "__main__":
--- vision/__init__.py
+++ vision/__init__.py
@@ -1,13 +1,19 @@
 import torch
 import numpy as np
 import cv2
+import os
+from dotenv import load_dotenv
 from PIL import Image, ImageDraw, ImageFont
 from ultralytics import YOLO, SAM
 from ultralytics.engine.results import Results
 from lavis.models import load_model_and_preprocess
 from pathlib import Path
 from typing import Literal
+from openai import OpenAI
 
+load_dotenv()
+
+openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 yolo_segmenter_model = YOLO("yolov8x-seg.pt")
@@ -15,127 +21,185 @@ sam_segmenter_model = SAM("mobile_sam.pt")
 yoloworld_segmenter_model = YOLO("yolov8x-worldv2.pt")
 
 
-def do_segment(
-    image: Path,
-    segmenter: Literal["yolo", "sam", "yoloworld"] = "yolo",
-    prompt: str = None,
-):
-    print("processsing image...")
-    if segmenter == "yolo":
-        results: list[Results] = yolo_segmenter_model.predict(image)
-        return results, yolo_segmenter_model
-    elif segmenter == "sam":
-        results: list[Results] = sam_segmenter_model.predict(image, max_det=15)
-        return results, sam_segmenter_model
-    elif segmenter == "yoloworld":
-        # object detection only; does not perform segmentation
-        if prompt:
-            yoloworld_segmenter_model.set_classes([prompt])
-        results = yoloworld_segmenter_model.predict(image)
-        return results, yoloworld_segmenter_model
+class Retina:
+    def __init__(
+        self, src_image: Path, segmenter: str = "yolo", prompt: str = None
+    ) -> None:
+        self.src_image = src_image
+        self.segmenter = segmenter
+        self.prompt = prompt
 
+    def generate_som_image(self):
+        """Returns a PIL Image."""
 
-def rescale_coordinates(original_width, original_height, new_width, new_height, x, y):
-    # Calculate the scaling factors for width and height
-    sf_w = new_width / original_width
-    sf_h = new_height / original_height
+        results, _ = self.do_segment(segmenter=self.segmenter, prompt=self.prompt)
+        som = self.set_marks(results, font_size=50)
+        return som
 
-    # Scale the coordinates using the scaling factor
-    new_x = int(sf_w * x)
-    new_y = int(sf_h * y)
+    def do_segment(
+        self,
+        segmenter: Literal["yolo", "sam", "yoloworld"] = "yolo",
+        prompt: str = None,
+    ):
+        print("processsing image...")
+        if segmenter == "yolo":
+            results: list[Results] = yolo_segmenter_model.predict(self.src_image)
+            return results, yolo_segmenter_model
+        elif segmenter == "sam":
+            results: list[Results] = sam_segmenter_model.predict(
+                self.src_image, max_det=15
+            )
+            return results, sam_segmenter_model
+        elif segmenter == "yoloworld":
+            # object detection only; does not perform segmentation
+            if prompt:
+                yoloworld_segmenter_model.set_classes([prompt])
+            results = yoloworld_segmenter_model.predict(self.src_image)
+            return results, yoloworld_segmenter_model
 
-    return new_x, new_y
+    def _rescale_coordinates(
+        self, original_width, original_height, new_width, new_height, x, y
+    ):
+        """
+        Rescale point coordinates from mask dimensions to desired image dimensions.
 
+        Args:
+            x, y: point coordinates to rescale
+        """
 
-def keep_largest_contour(binary_mask):
-    contours, hierarchy = cv2.findContours(
-        binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
+        # Calculate the scaling factors for width and height
+        sf_w = new_width / original_width
+        sf_h = new_height / original_height
 
-    max_area = 0
-    largest_contour = None
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > max_area:
-            max_area = area
-            largest_contour = contour
+        # Scale the coordinates using the scaling factor
+        new_x = int(sf_w * x)
+        new_y = int(sf_h * y)
 
-    # Create a new mask for the largest contour
-    largest_contour_mask = np.zeros_like(binary_mask)
-    if largest_contour is not None:
-        cv2.drawContours(
-            largest_contour_mask, [largest_contour], -1, (255), thickness=cv2.FILLED
+        return new_x, new_y
+
+    def _keep_largest_contour(self, binary_mask):
+        """
+        For mask with multiple parts, keep only the one with largest area for marking.
+        """
+
+        contours, hierarchy = cv2.findContours(
+            binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
 
-    return largest_contour_mask
+        max_area = 0
+        largest_contour = None
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > max_area:
+                max_area = area
+                largest_contour = contour
 
-
-def set_marks(image: Path, detections: list[Results], font_size: int = 20):
-    print("setting marks...")
-    raw_image = Image.open(image)
-    draw = ImageDraw.Draw(raw_image)
-    font = ImageFont.load_default(size=font_size)
-
-    for item in detections:
-        masks = item.masks.data
-        for i, mask in enumerate(masks):
-            mask_arr = mask.detach().cpu().numpy() * 255
-            src_mask = (mask_arr).astype("uint8")
-
-            current_mask = keep_largest_contour(src_mask)
-
-            mask_dt = cv2.distanceTransform(current_mask, cv2.DIST_L2, 0)
-            mask_dt = mask_dt[1:-1, 1:-1]
-            max_dist = np.max(mask_dt) * 0.8
-            coords_y, coords_x = np.where(mask_dt >= max_dist)
-
-            # calculate a center within image bounds
-            height, width = mask_dt.shape
-            center_x = coords_x[len(coords_x) // 2] + 2
-            center_y = coords_y[len(coords_y) // 2] - 6
-            center_x = max(0, min(center_x, width - 1))
-            center_y = max(0, min(center_y, height - 1))
-            center = (center_x, center_y)
-            x, y = center
-
-            # rescale center point coordinates to raw image size
-            mask_image = Image.fromarray(mask_arr)
-            scaled_center = rescale_coordinates(
-                mask_image.width,
-                mask_image.height,
-                raw_image.width,
-                raw_image.height,
-                x,
-                y,
+        # Create a new mask for the largest contour
+        largest_contour_mask = np.zeros_like(binary_mask)
+        if largest_contour is not None:
+            cv2.drawContours(
+                largest_contour_mask, [largest_contour], -1, (255), thickness=cv2.FILLED
             )
 
-            # add numerical label
-            text = str(i + 1)
-            text_width, text_height = draw.textbbox((0, 0), text, font=font)[2:]
-            text_background = Image.new(
-                "RGB", (text_width + 2, text_height + 2), "black"
-            )
-            draw = ImageDraw.Draw(text_background)
-            draw.text((0, 0), text, fill="white", font=font)
+        return largest_contour_mask
 
-            raw_image.paste(text_background, scaled_center)
+    def set_marks(self, detections: list[Results], font_size: int = 20):
+        print("setting marks...")
+        raw_image = Image.open(self.src_image)
+        draw = ImageDraw.Draw(raw_image)
+        font = ImageFont.load_default(size=font_size)
 
-    return raw_image
+        for item in detections:
+            masks = item.masks.data
+            for i, mask in enumerate(masks):
+                mask_arr = mask.detach().cpu().numpy() * 255
+                src_mask = (mask_arr).astype("uint8")
+
+                current_mask = self._keep_largest_contour(src_mask)
+
+                mask_dt = cv2.distanceTransform(current_mask, cv2.DIST_L2, 0)
+                mask_dt = mask_dt[1:-1, 1:-1]
+                max_dist = np.max(mask_dt) * 0.8
+                coords_y, coords_x = np.where(mask_dt >= max_dist)
+
+                # calculate a center within image bounds
+                height, width = mask_dt.shape
+                center_x = coords_x[len(coords_x) // 2] + 2
+                center_y = coords_y[len(coords_y) // 2] - 6
+                center_x = max(0, min(center_x, width - 1))
+                center_y = max(0, min(center_y, height - 1))
+                center = (center_x, center_y)
+                x, y = center
+
+                # rescale center point coordinates to image size
+                mask_image = Image.fromarray(mask_arr)
+                scaled_center = self._rescale_coordinates(
+                    mask_image.width,
+                    mask_image.height,
+                    raw_image.width,
+                    raw_image.height,
+                    x,
+                    y,
+                )
+
+                # add numerical label
+                text = str(i + 1)
+                text_width, text_height = draw.textbbox((0, 0), text, font=font)[2:]
+                text_background = Image.new(
+                    "RGB", (text_width + 2, text_height + 2), "black"
+                )
+                draw = ImageDraw.Draw(text_background)
+                draw.text((0, 0), text, fill="white", font=font)
+
+                raw_image.paste(text_background, scaled_center)
+
+        return raw_image
+
+    def write_caption(self):
+        print("writing image caption...")
+        raw_image = Image.open(self.src_image).convert("RGB")
+
+        model, vis_processors, _ = load_model_and_preprocess(
+            name="blip_caption", model_type="large_coco", is_eval=True, device=device
+        )
+        image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        caption = model.generate({"image": image})
+
+        return caption[0]
 
 
-def write_caption(img: Path):
-    print("writing image caption...")
-    raw_image = Image.open(img).convert("RGB")
+class Chat:
+    def __init__(self) -> None:
+        """Conversation with OpenAI GPT-4V"""
+        self.model = "gpt-4-vision-preview"
 
-    model, vis_processors, _ = load_model_and_preprocess(
-        name="blip_caption", model_type="large_coco", is_eval=True, device=device
-    )
-    image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-    caption = model.generate({"image": image})
+    def write_context(self, image_path: str, caption: str):
+        """Use GPT-4V"""
+        system_prompt = "You are a visual assistant, whose primary function is to accurately describe and analyze images, and respond to related questions. When describing an image, make sure to mention all key items, and spatial relationships. If asked a question about the image, make use of the number markings on the key items, and utilize your analysis to give a concise and applicable response. Maintain a friendly demeanor while interacting with users. Always strive for accuracy in your descriptions and responses, and use clear and simple language to ensure understanding by users of varying backgrounds."
+        initial_user_prompt = f"Image caption: {caption}\n\nThink step by step to write a description of the image (with visual grounding) using both the image and its caption."
 
-    return caption[0]
+        response = openai_client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": initial_user_prompt},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": image_path,
+                            },
+                        },
+                    ],
+                },
+            ],
+            max_tokens=1000,
+        )
+
+        return response.choices[0].message
 
 
-def write_context(img: Path, caption: str):
-    """Use GPT-4V"""
-    return
+def resize_image(image: Path):
+    pass
